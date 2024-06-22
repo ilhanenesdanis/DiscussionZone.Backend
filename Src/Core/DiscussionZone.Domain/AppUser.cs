6ad02174aefd7d116b361811e9be1f93860c2ce0
ilhan enes daniş <danisenes3@gmail.com>
2024-06-22 17:41:55 +0300

    Add project files.

---
+++ Src/Core/DiscussionZone.Domain/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DiscussionZone.Domain
+{
+    public sealed class AppUser : IdentityUser
+    {
+    }
+}
---
+++ Src/Core/DiscussionZone.Domain/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscussionZone.Domain.Base
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

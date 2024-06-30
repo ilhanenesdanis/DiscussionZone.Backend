17b3d6c92d458a5508f619699373cb6bb603c90e
ilhan enes daniş <danisenes3@gmail.com>
2024-06-30 13:46:41 +0300

    serilog logging and seq

---
+++ Src/API/DiscussionZone.API/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DiscussionZone.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController(ILogger<TestController> logger) : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger = logger;
+
+        [HttpGet]
+        public async Task<IActionResult> Test()
+        {
+            _logger.LogInformation("Loglama işlemi tamamlandı");
+            return Ok();
+        }
+    }
+}
--- Src/API/DiscussionZone.API/Program.cs
+++ Src/API/DiscussionZone.API/Program.cs
@@ -1,4 +1,7 @@
 using DiscussionZone.Persistence.IOC;
+using Serilog;
+using Serilog.Core;
+using Serilog.Sinks.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,14 +14,40 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddPersistenceService(builder.Configuration);
 
+
+Logger log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt")
+    .WriteTo.PostgreSQL(builder.Configuration.GetConnectionString("PostgreSQL"), "ApplicationLogs",
+                        needAutoCreateTable: true)
+    .Enrich.FromLogContext()
+    .MinimumLevel.Information()
+    .WriteTo.Seq(builder.Configuration.GetSection("SeqUrl")?.Value ?? string.Empty)
+    .CreateLogger();
+
+builder.Host.UseSerilog(log);
+
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+    logging.RequestHeaders.Add("sec-ch-ua");
+    logging.MediaTypeOptions.AddText("application/json");
+    logging.ResponseBodyLogLimit = 4096;
+    logging.RequestBodyLogLimit = 4096;
+});
+
 var app = builder.Build();
 
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpLogging();
 
 app.UseHttpsRedirection();
 
